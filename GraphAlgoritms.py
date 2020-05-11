b31d7853449980ce3c0e8362698e7cd60eafebf0
joaop21 <jpsilva9898@gmail.com>
2020-05-11 16:35:18 +0100

    Add Erdos-Renyi

--- GraphAlgoritms.py
+++ GraphAlgoritms.py
@@ -1,2 +1,24 @@
-def chitnisEtAl():
-    pass
+import matplotlib.pyplot as plt
+import networkx as nx
+from random import randrange
+
+
+# create a connected component with Erdos Renyi algorithm
+def erdosRenyi(num_vertices):
+    graph = nx.Graph()
+    for x in range(num_vertices):
+        graph.add_node(x)
+
+    while not nx.is_connected(graph):
+        i = randrange(num_vertices)
+        j = randrange(num_vertices)
+        if i != j:
+            graph.add_edge(i, j)
+
+    return graph
+
+
+if __name__ == "__main__":
+    G = erdosRenyi(20)
+    nx.draw(G, pos=nx.spring_layout(G))
+    plt.show()
