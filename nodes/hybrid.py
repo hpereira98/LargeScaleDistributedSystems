872b738b4ace92c7199faec81ea2bf146eb9ed76
João <jpsilva9898@gmail.com>
2020-05-22 11:48:37 +0100

    Push-Sum first approach (#14)
    * Reformat files due to pycharm upsetness
    
    * More reformats
    
    * Convergence with flooding
    
    * Fix a while loop

--- nodes/hybrid.py
+++ nodes/hybrid.py
@@ -2,20 +2,49 @@ from .node import Node
 
 from enum import Enum
 
-class HybridNode (Node):
+
+class HybridNode(Node):
 
     # - id: identification number 
     # - role: node role in the network
     # - neighbors: adjacent nodes id's
-    def __init__ (self, id, role, neighbors):
+    def __init__(self, id, role, distances, fanout, initial_value):
 
         self.id = id
         self.role = role
-        self.neighbors = neighbors
 
+        # aggregation info
+        self.sum = initial_value
+        self.weight = 0
+        self.aggregate = self.sum
 
-    def handle (self, src, data):
-        
+        # round info
+        self.message_id = 0
+        # dictionary for received messages in some round
+        self.received = {}
+
+        # setting neighbors
+        self.neighbors = []
+        for (src, dst) in distances.keys():
+            if src == self.id:
+                self.neighbors.append(dst)
+            elif dst == self.id:
+                self.neighbors.append(src)
+
+        # setting eager and lazy neighbors
+        fan = 1
+        self.eager = []
+        self.lazy = []
+        for neighbor in self.neighbors:
+            if fan <= fanout:
+                self.eager.append(neighbor)
+            else:
+                self.lazy.append(neighbor)
+            fan += 1
+
+    # invoked from simulator
+    def handle(self, src, data):
+
         # unpacking data 
         type, id, payload = data
 
@@ -44,34 +73,102 @@ class HybridNode (Node):
 
             return []
 
+    # multicast pair to neighbors
+    def __multicast__(self):
 
-    def __gossip__ (self, src, id, payload):
-        
-        return [self.role, "GOSSIP", payload]
+        res = []
 
-    def __ihave__ (self, src, id):  
-        
+        self.sum /= len(self.eager) + 1
+        self.weight /= len(self.eager) + 1
+
+        for node in self.eager:
+            res.append((node, (MessageType.GOSSIP, self.message_id, (GossipType.REQUEST, self.sum, self.weight)), 0))
+
+        for node in self.lazy:
+            res.append((node, (MessageType.IHAVE, self.message_id, ""), 0))
+
+        self.message_id += 1
+
+        return res
+
+    # send a message to one node
+    def __cast__(self, dst, message_id):
+
+        res = []
+
+        self.sum /= 2
+        self.weight /= 2
+
+        res.append((dst, (MessageType.GOSSIP, message_id, (GossipType.RESPONSE, self.sum, self.weight)), 0))
+
+        return res
+
+    # invoked when received a gossip message
+    def __gossip__(self, src, id, payload):
+
+        # triplet received
+        type, sum, weight = payload
+
+        # for returning
+        res = []
+
+        # case i'm the initial node
+        if src is None:
+            self.weight = 1
+            # print(str(self.id) + '(begin): \tsum: ' + str(round(self.sum, 5)) + ' \tweight: ' + str(round(self.weight, 5)) + ' \taggregate: ' + str(self.aggregate))
+
+        else:
+            # print(str(self.id) + '(begin): \tsum: ' + str(round(self.sum, 5)) + ' \tweight: ' + str(round(self.weight, 5)) + ' \taggregate: ' + str(self.aggregate))
+            if type is GossipType.REQUEST:
+                res = res + self.__cast__(src, id)
+            else:
+                self.received[id].append(src)
+
+            self.sum += sum
+            self.weight += weight
+
+        # multicast only when there isn't previous round and the current round isn't in the map
+        # or the previous round has finished
+        if ((self.message_id - 1) not in self.received and self.message_id not in self.received) or (
+                len(self.received[self.message_id - 1]) == len(self.neighbors)):
+            self.received[self.message_id] = []
+            res = res + self.__multicast__()
+
+        # print('send: ' + str(res))
+
+        self.aggregate = round(self.sum / self.weight, 3)
+        # print(str(self.id) + '(end): \tsum: ' + str(round(self.sum, 5)) + ' \tweight: ' + str(round(self.weight, 5)) + ' \taggregate: ' + str(self.aggregate))
+
+        return res
+
+    # invoked when received a ihave message
+    def __ihave__(self, src, id):
+
         return []
 
-    def __ack__ (self, src, id):
-        
+    # invoked when received an ack message
+    def __ack__(self, src, id):
+
         return []
 
-    def __ask__ (self, src, id):
-        
+    # invoked when received an ask message
+    def __ask__(self, src, id):
+
         return []
 
-    def __periodic__ (self, src, id):
-        
+    # invoked when received a periodic message
+    def __periodic__(self, src, id):
+
         return []
 
-    def __gc__ (self, src):
-        
+    # invoked when received a garbageCollection message
+    def __gc__(self, src):
+
         return []
+
 
 # different types of messages recognized by this type of node
 class MessageType(Enum):
-        
     GOSSIP = 1
     IHAVE = 2
     ASK = 3
@@ -79,8 +176,13 @@ class MessageType(Enum):
     GC = 5
     ACK = 6
 
+
+class GossipType(Enum):
+    REQUEST = 1
+    RESPONSE = 2
+
+
 # different types of hybrid node
-class HybridRole (Enum):
-        
+class HybridRole(Enum):
     PRIMARY = 1
     SECUNDARY = 2
--- sim/faulty.py
+++ sim/faulty.py
@@ -2,13 +2,14 @@ from .sim import DiscreteEventSimulator
 
 import random
 
-class FaultySimulator (DiscreteEventSimulator):
+
+class FaultySimulator(DiscreteEventSimulator):
 
     # - nodes: graph nodes
     # - distances: distances between each node 
     # - fault_chance: probability of losing a message in simulation
-    def __init__(self, nodes, distances, fault_chance = 0):
-        
+    def __init__(self, nodes, distances, fault_chance=0):
+
         # nodes
         self.nodes = nodes
 
@@ -19,54 +20,55 @@ class FaultySimulator (DiscreteEventSimulator):
         self.current_instant = 0
 
         # events pending execution [(instant, (src, dst, data))]
-        self.pending = []  
+        self.pending = []
 
         # probability of losing an event
         self.fault_chance = fault_chance
 
     def start(self, initial_data, initial_node):
-        
+
         # starting randomizer
         random.seed()
-        
+
         # creating first event [instant, (src, dst, data)]
         instant, src, dst, data = 0, None, initial_node, initial_data
-        
+
         # schedule first event 
-        self.pending.append( (instant, (src, dst, data )) )
-        
+        self.pending.append((instant, (src, dst, data)))
+
         # run the loop
         return self.__loop__()
 
     def __loop__(self):
-        
+
         # creating a sorted event list
         ordered_events = []
 
         # running loop
-        while len(self.pending) > 0:
+        while len(self.pending) > 0:  # and self.current_instant <= 1000:  # 1000ms maximum
 
             # getting the event with lowest instant
-            event = min (self.pending, key = lambda e:e[0])
+            event = min(self.pending, key=lambda e: e[0])
 
             # unfolding event properties
             instant, src, dst, data = event[0], event[1][0], event[1][1], event[1][2]
+
+            # simulator time
+            self.current_instant = instant
 
             # printing event
             # print(str(instant) + "s :: " + str(src) + " -> " + str(dst) + " :: " + str(data))
 
             # removing event from the queue
             self.pending.remove(event)
-        
-            # skipping event based on fault probability
-            if random.random() < self.fault_chance:
 
+            # skipping event based on fault probability
+            # if src != dst and random.random() < self.fault_chance:
                 # skipping iteration
-                continue
+                # continue
 
             # executing event if event is valid
-            if (src, dst) in self.distances or src is None:
-
+            if (src, dst) in self.distances or (dst, src) in self.distances or src is None:
                 # appending event to sorted event list
                 ordered_events.append(event)
 
@@ -76,7 +78,7 @@ class FaultySimulator (DiscreteEventSimulator):
         # returning sorted event list
         return ordered_events
 
-    def __exec__ (self, event):
+    def __exec__(self, event):
 
         # unfolding event properties
         instant, src, dst, data = event[0], event[1][0], event[1][1], event[1][2]
@@ -89,9 +91,13 @@ class FaultySimulator (DiscreteEventSimulator):
 
         # generating events for each data
         for (new_dst, new_data, delay) in new_datas:
-
             # get distance to node
-            distance = self.distances[(new_src, new_dst)]
+            distance = 0
+            if (new_src, new_dst) in self.distances:
+                distance = self.distances[(new_src, new_dst)]
+
+            elif (new_dst, new_src) in self.distances:
+                distance = self.distances[(new_dst, new_src)]
 
             # creating new event
             new_event = (instant + distance + delay, (new_src, new_dst, new_data))
