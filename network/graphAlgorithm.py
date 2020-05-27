594dda1112dcda963f3ca35be5db43035fdb30d7
joaop21 <jpsilva9898@gmail.com>
2020-05-27 19:14:05 +0100

    Produce results

--- network/graphAlgorithm.py
+++ network/graphAlgorithm.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 from random import randrange
+from network.probabilities import calculate_probability, preferential_attachment
 
 
 # create a connected component with Erdos Renyi algorithm
@@ -12,6 +13,22 @@ def erdosRenyi(num_vertices):
     while not nx.is_connected(graph):
         i = randrange(num_vertices)
         j = randrange(num_vertices)
+        if i != j:
+            graph.add_edge("(" + str(i) + ")", "(" + str(j) + ")")
+
+    return graph
+
+
+# create a connected component with Barabasi Albert algorithm
+def barabasiAlbert(num_vertices):
+    graph = nx.Graph()
+    for x in range(num_vertices):
+        graph.add_node("(" + str(x) + ")")
+
+    while not nx.is_connected(graph):
+        probabilities = calculate_probability(graph, num_vertices)
+        i = preferential_attachment(num_vertices, probabilities)
+        j = preferential_attachment(num_vertices, probabilities)
         if i != j:
             graph.add_edge("(" + str(i) + ")", "(" + str(j) + ")")
 
---
+++ network/graphtype.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class GraphType(Enum):
+    ERDOS_RENYI = 1
+    BARABASI_ALBERT = 2
