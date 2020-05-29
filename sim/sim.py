d7e72cce68b4437620f9c60aa679ebd18fa105d5
joaop21 <jpsilva9898@gmail.com>
2020-05-29 01:53:02 +0100

    Simulator folder

--- sim/sim.py
+++ sim/sim.py
@@ -1,5 +1,12 @@
 class DiscreteEventSimulator:
+    """
+    Interface that declares abstract methods for implementations classes to implement.
+    """
 
     # returns list of events ordered by time
     def start(self, initial_data, initial_node):
+        pass
+
+    # returns list of events ordered by time, after an additional simulation time
+    def proceed(self, additional_simulation_time):
         pass
