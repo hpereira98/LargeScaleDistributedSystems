57dafae975efd5ee6354fde5f2a2424b995188d3
joaop21 <jpsilva9898@gmail.com>
2020-05-29 17:03:54 +0100

    Change algorithm of draw plot

--- benchmark/statisticsproducer.py
+++ benchmark/statisticsproducer.py
@@ -25,17 +25,20 @@ def calculate_points(dictionary):
     return x, y
 
 
-def draw_plot(erdos, barabasi, watts):
+def draw_plot(*args):
+    """
+    Draw a plot of time and message complexities given a bunch of arrays
+    """
+    
     fig, axs = plt.subplots(nrows=1, ncols=2, constrained_layout=False, figsize=(20, 10))
-    axs[0].plot(erdos[0], erdos[1], label='Erdos-Renyi')
-    axs[0].plot(barabasi[0], barabasi[1], label='Barabasi-Albert')
-    axs[0].plot(watts[0], watts[1], label='Watts-Strogatz')
+
+    for elem in args:
+        axs[0].plot(elem[1][0], elem[1][1], label=elem[0])
+        axs[1].plot(elem[1][2], elem[1][3], label=elem[0])
+
     axs[0].set(xlabel='Vertices', ylabel='Time (ms)', title='Time To Converge')
     axs[0].legend()
 
-    axs[1].plot(erdos[2], erdos[3], label='Erdos-Renyi')
-    axs[1].plot(barabasi[2], barabasi[3], label='Barabasi-Albert')
-    axs[1].plot(watts[2], watts[3], label='Watts-Strogatz')
     axs[1].set(xlabel='Vertices', ylabel='Messages', title='Messages To Converge')
     axs[1].legend()
 
@@ -44,9 +47,9 @@ def draw_plot(erdos, barabasi, watts):
 
 if __name__ == '__main__':
 
-    erdos = ([2, 4, 8, 16, 32, 64, 128], [130.0, 419.0, 769.0, 2235.0, 2201.0, 1721.0, 1440.0], [2, 4, 8, 16, 32, 64, 128], [37, 616.6, 3087.4, 18842.8, 37361.8, 62506, 108980.2])
-    barabasi = ([2, 4, 8, 16, 32, 64, 128], [130.0, 418.0, 850.0, 954.0, 1352.0, 770.0, 910.0], [2, 4, 8, 16, 32, 64, 128], [37, 602.2, 3624.4, 8789.2, 25251.4, 29965, 69655])
-    watts = ([2, 4, 8, 16, 32, 64, 128], [130.0, 250.0, 298.0, 361.0, 956.0, 3074.0, 5912.0], [2, 4, 8, 16, 32, 64, 128], [37, 717.4, 1703.8, 4085.2, 22603.6, 134516.8, 495046])
+    erdos = ("Erdos-Renyi", ([2, 4, 8, 16, 32, 64, 128], [130.0, 419.0, 769.0, 2235.0, 2201.0, 1721.0, 1440.0], [2, 4, 8, 16, 32, 64, 128], [37, 616.6, 3087.4, 18842.8, 37361.8, 62506, 108980.2]))
+    barabasi = ("Barabasi-Albert", ([2, 4, 8, 16, 32, 64, 128], [130.0, 418.0, 850.0, 954.0, 1352.0, 770.0, 910.0], [2, 4, 8, 16, 32, 64, 128], [37, 602.2, 3624.4, 8789.2, 25251.4, 29965, 69655]))
+    watts = ("Watts-Strogatz", ([2, 4, 8, 16, 32, 64, 128], [130.0, 250.0, 298.0, 361.0, 956.0, 3074.0, 5912.0], [2, 4, 8, 16, 32, 64, 128], [37, 717.4, 1703.8, 4085.2, 22603.6, 134516.8, 495046]))
 
     draw_plot(erdos, barabasi, watts)
 
