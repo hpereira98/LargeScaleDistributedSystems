a57fad04d6550298df37eaad24b2787ade3cfc1f
joaop21 <jpsilva9898@gmail.com>
2020-05-29 15:40:11 +0100

    Documenting network folder

--- network/graphtype.py
+++ network/graphtype.py
@@ -2,7 +2,11 @@ from enum import Enum
 
 
 class GraphType(Enum):
+    """ Different types of Graphs can be used.
+
+    Arguments:
+        Enum {Enumeration} -- type of the Graph Type
+    """
     ERDOS_RENYI = 1
     BARABASI_ALBERT = 2
-    RANDOM_GEOMETRIC = 3
-    WATTS_STROGATZ = 4
+    WATTS_STROGATZ = 3
